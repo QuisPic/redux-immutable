9c1bf98783797d556fc7282af21f9f6847bc6df9
QuisPic <56075863+QuisPic@users.noreply.github.com>
2020-07-01 16:10:07 +0300

    Added dist/

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import _combineReducers from './combineReducers';
+export { _combineReducers as combineReducers };
+//# sourceMappingURL=index.js.map
---
+++ dist/utilities/getStateName.js
@@ -0,0 +1,5 @@
+export default (function (action) {
+  return action && action.type === '@@redux/INIT' ? 'initialState argument passed to createStore' : 'previous state received by the reducer';
+});
+module.exports = exports['default'];
+//# sourceMappingURL=getStateName.js.map
